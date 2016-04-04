18dc19ea85dcec85b6cc255832d56cc2896738db
krustnic <avmityakov@gmail.com>
2016-04-04 14:41:28 +0000

    start adding web panel

--- public/config/tasks.php
+++ public/config/tasks.php
@@ -1,9 +1,14 @@
 <?php
 return [
+    // Username and password for web panel
+    "username" => "deployer",
+    "password" => "password",
+    
     "git-path" => "/usr/bin/git", // Path to git command
     "tasks"    => [        
         // Task name. Would be used in GET parameter at Webhook URL
-        "task1" => [
+        "task1" => [            
+            "description" => "Project X deploy", // Description for web panel interface
             "password"  => "password",
             "branch"    => "origin/master", // Branch name to hard reset from
             "git-dir"   => "/var/www/site1/.git", // Path to .git of your project (.git is included)
